a0ed2a1a769828f6cf6034f5d59411711f2f2118
kittiu <kittiu@kittiu>
2016-02-06 23:27:33 +0700

    [ADD] init addons

---
+++ account_budget_operating_unit/models/account_budget.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Jordi Ballester (Eficent)
+#    Copyright 2015 Eficent
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning
+
+
+class CrossoveredBudget(models.Model):
+    _inherit = "crossovered.budget"
+
+    operating_unit_id = fields.Many2one(
+        'operating.unit',
+        string='Operating Unit',
+        default=lambda self: self.env['res.users'].
+        operating_unit_default_get(self._uid),
+    )
+
+    @api.one
+    @api.constrains('operating_unit_id', 'company_id')
+    def _check_company_operating_unit(self):
+        if self.company_id and self.operating_unit_id and \
+                self.company_id != self.operating_unit_id.company_id:
+            raise Warning(_('The Company in the Move Line and in the '
+                            'Operating Unit must be the same.'))
+
+
+class CrossoveredBudgetLines(models.Model):
+    _inherit = "crossovered.budget.lines"
+
+    operating_unit_id = fields.Many2one(
+        'operating.unit',
+        related='crossovered_budget_id.operating_unit_id',
+        string='Operating Unit', readonly=True, store=True,
+    )
+
+    # DONE but NOT USED YET
+#     @api.one
+#     @api.constrains('operating_unit_id', 'analytic_account_id')
+#     def _check_analytic_account_operating_unit(self):
+#         if self.type not in ('payment', 'receipt'):
+#             return True
+#         if (
+#             self.analytic_account_id and self.operating_unit_id and
+#             self.analytic_account_id.operating_unit_id and
+#             self.analytic_account_id.operating_unit_idid !=
+#                 self.operating_unit_id.id
+#         ):
+#             raise Warning(_('The Analytic Account must have the same '
+#                             'Operating Unit as the one indicated in the '
+#                             'budget plan'))
+#         return True
---
+++ account_voucher_operating_unit/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Jordi Ballester (Eficent)
+#    Copyright 2015 Eficent
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': 'Vouchers with Operating Units',
+    'version': '1.0',
+    'category': 'Generic Modules/Sales & Purchases',
+    'description': '''
+Vouchers with Operating Units
+=============================
+This module introduces the following features:
+- Adds the OU as a required field in the payment vouchers
+- The voucher can only select AR o AP items for payment that belong to the
+same OU
+- Security rules are defined to ensure that users can only display the
+Vouchers in which they are allowed access to.
+''',
+    'author': "Eficent",
+    'website': 'http://www.eficent.com',
+    'depends': ['account_voucher_move_line_create_hooks',
+                'account_operating_unit', 'operating_unit'],
+    'data': [
+        'views/account_voucher_payment_receipt_view.xml',
+        'views/account_voucher_sales_purchase_view.xml',
+        'security/account_voucher_security.xml'
+    ],
+    'installable': True,
+}
