c79ba59ac18994396595fb721e4790af638542c1
kittiu <kittiu@kittiu>
2016-02-08 21:47:09 +0700

    [ENH] account_voucher_operating_unit

---
+++ account_voucher_operating_unit/tests/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# - Jordi Ballester Alomar
+# © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from . import test_account_voucher_operating_unit
+from . import test_payment_operating_unit
