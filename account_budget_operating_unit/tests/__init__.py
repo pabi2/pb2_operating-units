051b7abe1ef37fc5abd18aec404b6291bc430248
Kitti U. <kittiu@gmail.com>
2016-04-04 19:46:01 +0700

    -

--- account_budget_operating_unit/tests/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# - Jordi Ballester Alomar
-# © 2016 Serpent Consulting Services Pvt. Ltd. - Sudhir Arya
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import test_budget_operating_unit
-from . import test_budget_security
