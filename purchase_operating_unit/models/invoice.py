f1f9ee3aa73b6b3419b9586abc35fe48798df12b
Kitti U. <kittiu@gmail.com>
2016-07-07 10:09:28 +0700

    [FIX] purchase_operating_unit, fix case create Invoice by Order Line

--- purchase_operating_unit/models/invoice.py
+++ purchase_operating_unit/models/invoice.py
@@ -11,7 +11,7 @@ class AccountInvoice(models.Model):
 
     # kittiu: this purchase_order_change not exists in V8 it is from V8
     # PR at https://github.com/Eficent/operating-unit/pull/42
-#     # Load all unsold PO lines
+    # Load all unsold PO lines
 #     @api.onchange('purchase_id')
 #     def purchase_order_change(self):
 #         """
@@ -21,3 +21,10 @@ class AccountInvoice(models.Model):
 #             # Assign OU from PO to Invoice
 #             self.operating_unit_id = self.purchase_id.operating_unit_id.id
 #         return super(AccountInvoice, self).purchase_order_change()
+
+    @api.model
+    def create(self, vals):
+        if self._context.get('operating_unit_id', False):
+            operating_unit_id = self._context['operating_unit_id']
+            vals.update({'operating_unit_id': operating_unit_id})
+        return super(AccountInvoice, self).create(vals)
