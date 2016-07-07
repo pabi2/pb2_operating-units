965637f8eee5a10f5295a606baedad2ff660115c
Kitti U. <kittiu@gmail.com>
2016-07-07 09:28:09 +0700

    Propose to remove a file in https://github.com/Eficent/operating-unit/pull/42

--- purchase_operating_unit/models/invoice.py
+++ purchase_operating_unit/models/invoice.py
@@ -9,13 +9,15 @@ from openerp import api, models
 class AccountInvoice(models.Model):
     _inherit = 'account.invoice'
 
-    # Load all unsold PO lines
-    @api.onchange('purchase_id')
-    def purchase_order_change(self):
-        """
-        Override to add Operating Unit from Purchase Order to Invoice.
-        """
-        if self.purchase_id and self.purchase_id.operating_unit_id:
-            # Assign OU from PO to Invoice
-            self.operating_unit_id = self.purchase_id.operating_unit_id.id
-        return super(AccountInvoice, self).purchase_order_change()
+    # kittiu: this purchase_order_change not exists in V8 it is from V8
+    # PR at https://github.com/Eficent/operating-unit/pull/42
+#     # Load all unsold PO lines
+#     @api.onchange('purchase_id')
+#     def purchase_order_change(self):
+#         """
+#         Override to add Operating Unit from Purchase Order to Invoice.
+#         """
+#         if self.purchase_id and self.purchase_id.operating_unit_id:
+#             # Assign OU from PO to Invoice
+#             self.operating_unit_id = self.purchase_id.operating_unit_id.id
+#         return super(AccountInvoice, self).purchase_order_change()
